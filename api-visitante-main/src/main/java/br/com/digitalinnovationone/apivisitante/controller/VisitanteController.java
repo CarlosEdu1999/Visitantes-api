804e9898cb25f844edd1c3e49e4e35c84c63367a
Carlos Eduardo <81891515+CarlosEdu1999@users.noreply.github.com>
2021-08-26 15:00:29 -0300

    Add files via upload

---
+++ api-visitante-main/src/main/java/br/com/digitalinnovationone/apivisitante/controller/VisitanteController.java
@@ -0,0 +1,30 @@
+package br.com.digitalinnovationone.apivisitante.controller;
+
+import br.com.digitalinnovationone.apivisitante.dto.VisitanteRequestDto;
+import br.com.digitalinnovationone.apivisitante.entity.Visitante;
+import br.com.digitalinnovationone.apivisitante.service.VisitanteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/visitantes")
+@RequiredArgsConstructor
+public class VisitanteController {
+
+    private final VisitanteService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Visitante criar(@RequestBody VisitanteRequestDto dto) {
+        return service.criar(dto);
+    }
+
+    @GetMapping
+    public List<Visitante> listar() {
+        return service.listar();
+    }
+
+}
